48f9f71a34f972f47dae078b16c2dc7251613f36
nuris <nreyesq@alumnos.utalca.cl>
2015-04-30 00:32:00 -0300

    estilos botones iniciar sesion
    drawable/ButtonSignInStyle= estilo del boton de iniciar sesion naranjo
    drawable/ButtonSignInMailStyle= estilo del boton de iniciar sesion con
    mail verde
    drawable/ProgressBarStyle= estilo de la barra de progreso

--- HermesLogin/Resources/Resource.designer.cs
+++ HermesLogin/Resources/Resource.designer.cs
@@ -45,10 +45,16 @@ namespace HermesLogin
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int ButtonSignInMailStyle = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int monoandroidsplash = 2130837505;
+			public const int ButtonSignInStyle = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Icon = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int ProgressBarStyle = 2130837507;
 			
 			static Drawable()
 			{
@@ -63,20 +69,20 @@ namespace HermesLogin
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060001
-			public const int btnSignIn = 2131099649;
+			// aapt resource value: 0x7f050001
+			public const int btnSignIn = 2131034113;
 			
-			// aapt resource value: 0x7f060003
-			public const int btnSignInEmail = 2131099651;
+			// aapt resource value: 0x7f050003
+			public const int btnSignInEmail = 2131034115;
 			
-			// aapt resource value: 0x7f060004
-			public const int progressBar1 = 2131099652;
+			// aapt resource value: 0x7f050004
+			public const int progressBar1 = 2131034116;
 			
-			// aapt resource value: 0x7f060002
-			public const int textOr = 2131099650;
+			// aapt resource value: 0x7f050002
+			public const int textOr = 2131034114;
 			
-			// aapt resource value: 0x7f060000
-			public const int txtCreateAcount = 2131099648;
+			// aapt resource value: 0x7f050000
+			public const int txtCreateAcount = 2131034112;
 			
 			static Id()
 			{
@@ -119,22 +125,6 @@ namespace HermesLogin
 			}
 			
 			private String()
-			{
-			}
-		}
-		
-		public partial class Style
-		{
-			
-			// aapt resource value: 0x7f050000
-			public const int Mono_Android_Theme_Splash = 2131034112;
-			
-			static Style()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Style()
 			{
 			}
 		}
